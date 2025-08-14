9730b919e0e4ff359dcb92cf14fe672f4f222ac9
Desire <desireirankundawilliams@gmail.com>
2025-08-14 20:11:24 +0200

    able to login

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import "@mantine/core/styles.css";
 import "./App.css";
 import { MantineProvider } from "@mantine/core";
 import { Login } from "./auth/Login";
+import { Route, Routes } from "react-router-dom";
+import Overview from "./dashboard/Overview";
 
 function App() {
   return (
     <MantineProvider>
-      <Login />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/overview" element={<Overview />} />
+        {/* <Route path="/overview" element={<div>Overview Page</div>} /> */}
+      </Routes>
     </MantineProvider>
   );
 }
--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -11,20 +11,34 @@ import {
 import classes from "./auth.module.css";
 import { login } from "../api";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 export function Login() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    login(email, password)
+  const handleSubmit = () => {
+    fetch("http://localhost:4000/api/auth/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
       .then((data) => {
         console.log(data);
-      })
-      .catch((err) => {
-        setError(err.message);
+        if (data.success) {
+          localStorage.setItem("token", data.token);
+          navigate("/overview");
+        }
+        setError(data.message);
       });
   };
   return (
@@ -40,14 +54,14 @@ export function Login() {
           size="md"
           radius="md"
           value={email}
-          onChange={(event) => setEmail(event.currentTarget.value)}
+          onChange={(event) => setEmail(event.target.value)}
           mt="md"
           error={error ? "Invalid email or password" : null}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              handleSubmit(event);
-            }
-          }}
+          // onKeyDown={(event) => {
+          //   if (event.key === "Enter") {
+          //     handleSubmit(event);
+          //   }
+          // }}
         />
         <PasswordInput
           label="Password"
@@ -56,35 +70,20 @@ export function Login() {
           size="md"
           radius="md"
           value={password}
-          onChange={(event) => setPassword(event.currentTarget.value)}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              handleSubmit(event);
-            }
-          }}
+          onChange={(event) => setPassword(event.target.value)}
+          // onKeyDown={(event) => {
+          //   if (event.key === "Enter") {
+          //     handleSubmit(event);
+          //   }
+          // }}
         />
-        <PasswordInput
-          label="Password"
-          placeholder="Your password"
-          mt="md"
-          size="md"
-          radius="md"
-          value={password}
-          onChange={(event) => setPassword(event.currentTarget.value)}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              handleSubmit(event);
-            }
-          }}
-        />
+
         <Checkbox label="Keep me logged in" mt="xl" size="md" />
-        <Button fullWidth mt="xl" size="md" radius="md" onClick={handleSubmit}>
-          Login
-        </Button>
+        <Button onClick={handleSubmit}>Login</Button>
 
         <Text ta="center" mt="md">
           Don&apos;t have an account?{" "}
-          <Anchor href="#" fw={500} onClick={(event) => event.preventDefault()}>
+          <Anchor href="#" fw={500} onClick={handleSubmit}>
             Register
           </Anchor>
         </Text>
